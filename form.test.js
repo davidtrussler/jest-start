383290b9dcad4c7df2def2070ec5f72e7739eade
David Trussler <mail@davidtrussler.net>
2020-07-15 21:13:21 +0100

    Adds markup as external file

--- form.test.js
+++ form.test.js
@@ -2,42 +2,27 @@
  * @jest-environment jsdom
  */
 
-// const form = require('./form');
-// import {bindEvents, clickBtn} from './form';
 const { bindEvents, clickBtn, insertText } = require('./form')
+const { markup } = require('./markup')
 
-// console.log(form); 
+console.log(typeof markup);
 
-// const fs = require('fs');
-// const path = require('path');
-// const html = fs.readFileSync(path.resolve(__dirname, './form.htm'), 'utf8');
-// jest.dontMock('fs'); 
 
-test('inserting content', () => {
- document.body.innerHTML = `
-    <form>
-			<label for="this">enter text</label>
-			<input type="text" id="this">
-			<p id="result"></p>
-			<button type="button" id="button">Click me</button>
-		</form>
-  `;
+describe('inserting content', () => {
+ document.body.innerHTML = markup;
 
-  bindEvents();
+
+  beforeEach(() => {
+    bindEvents();
+  });
+
 
   insertText();
 
+  test('It copies content of input to another element', () => {
     document.getElementsByTagName('input').item(0).value = 'shut up';
 
-
-  console.log(document.getElementsByTagName('input').item(0));
-
-  // Array.prototype.slice.call(document.getElementsByTagName('input'))[0].value = 'shut up';
-  // document.getElementsByTagName('input').item(0).value = 'shut up';
-
-  // Array.prototype.slice.call(document.getElementsByTagName('button'))[0].click();
-  document.getElementById('button').click();
-  // expect(document.getElementById('this').value).toBe('shut up');
-
-  expect(document.getElementById('result').textContent).toBe('shut up');
+    document.getElementById('button').click();
+    expect(document.getElementById('result').textContent).toBe('shut up');
+  }); 
 });
